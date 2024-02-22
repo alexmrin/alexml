55f7ca4bd3430d31af4e6474c82c5edb6c201b2a
Alex <118281736+alexmrin@users.noreply.github.com>
2024-02-21 20:19:15 -0800

    resnet50

---
+++ alexml/models/resnet50.py
@@ -0,0 +1,96 @@
+import torch.nn as nn
+import torch
+
+class Resnet50(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2),
+            nn.BatchNorm2d(64),
+            nn.SiLU(inplace=True),
+            nn.MaxPool2d(stride=2, kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            ResBlock(64, 128, 32),
+            ResBlock(128, 128, 32),
+            ResBlock(128, 128, 32),
+        )
+        self.conv3 = nn.Sequential(
+            ResBlock(128, 256, 64, downsample=True),
+            ResBlock(256, 256, 64),
+            ResBlock(256, 256, 64),
+            ResBlock(256, 256, 64),
+        )
+        self.conv4 = nn.Sequential(
+            ResBlock(256, 512, 128, downsample=True),
+            ResBlock(512, 512, 128),
+            ResBlock(512, 512, 128),
+            ResBlock(512, 512, 128),
+            ResBlock(512, 512, 128),
+        )
+        self.conv5 = nn.Sequential(
+            ResBlock(512, 1024, 256, downsample=True),
+            ResBlock(1024, 1024, 256),
+            ResBlock(1024, 1024, 256),
+        )
+        self.pooling = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(1024, 1000),
+            nn.BatchNorm1d(1000),
+            nn.SiLU(inplace=True),
+            nn.Dropout1d(p=0.5),
+            nn.Linear(1000, 10),
+        )
+
+    def forward(self, x) -> torch.Tensor:
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.conv5(out)
+        return out
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, squeeze_channels, downsample=False):
+        super().__init__()
+        if downsample:
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(in_channels, squeeze_channels, kernel_size=1, stride=2),
+                nn.BatchNorm2d(squeeze_channels),
+                nn.SiLU(inplace=True)
+            )
+        else:
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(in_channels, squeeze_channels, kernel_size=1),
+                nn.BatchNorm2d(squeeze_channels),
+                nn.SiLU(inplace=True)
+            )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(squeeze_channels, squeeze_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(squeeze_channels),
+            nn.SiLU(inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(squeeze_channels, out_channels, kernel_size=1),
+            nn.BatchNorm2d(out_channels),
+            nn.SiLU(inplace=True)
+        )
+        if downsample:
+            self.identtiy = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1)
+            )
+        else:
+            self.identity = nn.Sequential()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        return out + self.identity(x)
+    
+def main(): 
+    model = Resnet50()
+    print(sum(p.numel() for p in model.parameters()))
+    
+if __name__ == "__main__":
+    main()
